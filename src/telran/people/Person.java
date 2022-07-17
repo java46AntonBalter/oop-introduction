5e82c2f591a217ea2b552a38b433f7a19c0f4736
Anton Balter <antbalter@gmail.com>
2022-07-17 23:01:00 +0300

    HW09-solution

--- src/telran/people/Person.java
+++ src/telran/people/Person.java
@@ -7,18 +7,13 @@ public class Person {
 
 	public Person(long id, int birthYear, String email) {
 		setEmail(email);
-		setId(id);
-		setBirthYear(birthYear);
+		this.id = id;
+		this.birthYear = birthYear;
 	}
 	public void setEmail(String email) {
 		this.email = email;		
 	}
-	public void setId(long id) {
-		this.id = id;		
-	}
-	public void setBirthYear(int birthYear) {
-		this.birthYear = birthYear;		
-	}
+	
 	public String getEmail() {
 		return email;		
 	}
