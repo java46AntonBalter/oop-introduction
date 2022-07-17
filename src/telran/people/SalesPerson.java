1098a67f02cfd5bf0865dc977fc83334890d41c0
Anton Balter <antbalter@gmail.com>
2022-07-17 23:08:20 +0300

    HW09-solution

--- src/telran/people/SalesPerson.java
+++ src/telran/people/SalesPerson.java
@@ -2,21 +2,21 @@ package telran.people;
 
 public class SalesPerson extends Employee {
 	private int sales;
-	private int percent;
-	public SalesPerson (long id, int birthYear, String email, int basicSalary, int sales, int percent) {
+	private int percentPay;
+	public SalesPerson (long id, int birthYear, String email, int basicSalary, int sales, int percentPay) {
 		super(id, birthYear, email, basicSalary);
 		setSales(sales);
-		setPercent(percent);
+		setPercentPay(percentPay);
 	}
 	public void setSales(int sales) {
 		this.sales = sales;		
 	}
-	public void setPercent(int percent) {
-		this.percent = percent;		
+	public void setPercentPay(int percentPay) {
+		this.percentPay = percentPay;		
 	}
 	@Override
 	public int computePay() {
-		return super.computePay() + ((sales/100) * percent);
+		return super.computePay() + (int)Math.round(sales/100.0 * percentPay);
 	}
 	
 
