c659618dceacfe30a45f9bd4c25cbb8d90aa30a1
Anton Balter <antbalter@gmail.com>
2022-07-21 18:23:40 +0300

    CW10

--- src/telran/people/CompanySortedArray.java
+++ src/telran/people/CompanySortedArray.java
@@ -4,37 +4,27 @@ import java.util.Arrays;
 
 public class CompanySortedArray extends CompanyArray {
 	@Override
-	public boolean addEmployee(Employee empl) {
-		long emplId = empl.getId();
-		int emplIndex = getEmployeeIndex(emplId);
-
-		if (emplIndex >= 0) {
+public boolean addEmployee(Employee empl) {
+		
+		//calls getEmployeeIndex
+		int ind = getEmployeeIndex(empl.getId());
+		if (ind >= 0) {
 			return false;
 		}
-
-		emplIndex = -(emplIndex + 1);
-
-		Employee[] newArr = new Employee[employees.length + 1];
-
-		for (int i = 0; i < newArr.length; i++) {
-			if (i < emplIndex) {
-				newArr[i] = employees[i];
-			} else if (i == emplIndex) {
-				newArr[i] = empl;
-			} else {
-				newArr[i] = employees[i - 1];
-			}
-		}
-
-		employees = newArr;
-		return true;
-	}
-
-	@Override
+		ind = -(ind + 1);
+		employees = Arrays.copyOf(employees, employees.length + 1);
+		System.arraycopy(employees, ind, employees, ind + 1, employees.length - 1 - ind);
+		employees[ind] = empl;
+	return true;
+		
+}
+	
+	@Override 
 	protected int getEmployeeIndex(long id) {
-		return Arrays.binarySearch(employees, new Person(id, 0, null));
+	
+		Person pattern = new Person(id, 0, null);
+		return Arrays.binarySearch(employees, pattern);
 	}
-
 	@Override
 	public Employee[] getAllEmployees() {
 		return Arrays.copyOf(employees, employees.length);
